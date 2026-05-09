02a4f4858636e8395169809035cae9eb9b7715d3
hrwnt0 <isco.alarcon@europe.com>
2026-05-09 12:22:48 +0800

    Polish HIRA Nothing UI on Android

--- pwa/service-worker.js
+++ pwa/service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "hira-os-v75";
-const HIRA_APP_VERSION = "20260509-nothing-2";
+const CACHE_NAME = "hira-os-v76";
+const HIRA_APP_VERSION = "20260509-nothing-3";
 const ASSETS = [
   "/",
-  "/styles.css?v=20260509-nothing-2",
-  "/app.js?v=20260509-nothing-2",
+  "/styles.css?v=20260509-nothing-3",
+  "/app.js?v=20260509-nothing-3",
   "/static/icon.svg",
   "/manifest.webmanifest"
 ];
